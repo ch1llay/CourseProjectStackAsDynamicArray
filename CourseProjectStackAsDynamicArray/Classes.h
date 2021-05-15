efe3494905ca5db439668e133c041c678fc628bb
ch1llay <ch1llay@yandex.ru>
2021-05-15 19:11:01 +0300

    commit

--- CourseProjectStackAsDynamicArray/Classes.h
+++ CourseProjectStackAsDynamicArray/Classes.h
@@ -27,9 +27,43 @@ public:
 	{
 		cout << "Destructor Stack" << endl;
 	}
-	void virtual push(int n);
-	int virtual pop();
-	void virtual show();
-	void virtual notFull();
-	void virtual notEmpty();
+	void virtual push(int) = 0;
+	int virtual pop() = 0;
+	void virtual show() = 0;
+	bool notFull() {
+		return rightTop - leftTop > 1;
+	}
+	bool notEmpty() {
+		return rightTop - leftTop <= d.N;
+	}
+protected:
+	static DynamicArray d;
+	int leftTop, rightTop;
+};
+
+class LeftStack:public Stack {
+private:
+	int leftTop = -1;
+public:
+	LeftStack() {
+
+		cout << "Constructor LeftStack" << endl;
+	}
+	~LeftStack()
+	{
+		cout << "Destructor LeftStack" << endl;
+	}
+	void push(int n) override {
+		if (notFull())
+			d.arr[leftTop++] = n;
+		else
+			cout << "stack is full";
+	}
+	int pop() override {
+		if (notEmpty())
+			return d.arr[--leftTop];
+		else
+			throw;
+	}
+	int 
 };
